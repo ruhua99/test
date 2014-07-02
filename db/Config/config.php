1d9792177980fcccf6e9e6c90725bfc7567584b2
ruhua99 <125113598@qq.com>
2014-07-02 20:26:55 +0800

    fds
    sdfs

---
+++ db/Config/config.php
@@ -0,0 +1,15 @@
+<?php
+if (!defined("HDPHP_PATH"))exit('No direct script access allowed');
+//更多配置请查看hdphp/Config/config.php
+return array(
+	"DEBUG_SHOW" =>TRUE,
+	"DB_DRIVER"  =>"mysqli",
+	"DB_CHARSET" =>"utf8", 
+	"DB_HOST"    =>"127.0.0.1",
+	"DB_PORT"    =>3306,
+	"DB_USER"    =>"root",
+	"DB_PASSWORD"=>"root",
+	"DB_DATABASE"=>"v5",
+	"DB_PRI"     =>""
+);
+?>
---
+++ db/Control/NewsControl.class.php
@@ -0,0 +1,9 @@
+<?php
+class NewsControl extends Control{
+	function show(){
+		echo 'hello';
+	}
+	function add(){
+		$this->display();
+	}
+}
